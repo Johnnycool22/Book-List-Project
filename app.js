8a86d97437faa75034305856c92c3433d323efff
John <jkforsythe@hotmail.com>
2019-12-01 15:20:39 +0000

    Show alert method and text created

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ UI.prototype.addBookToList = function(book) {
   list.appendChild(row);
 }
 
+// Show alert
+UI.prototype.showAlert = function(message, className) {
+  // Create a div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+  // Timeout after 3 secs
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -44,11 +64,18 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // Instantiate UI
   const ui = new UI();
 
-  // Add book to list
+  // Validate
+  if(title === '' || author === '' || isbn === '') {
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
   ui.addBookToList(book);
 
   // Clear fields
   ui.clearFields();
+
+  }
 
   e.preventDefault();
 });
