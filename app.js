e15bc085af00716ef2783395ef84096d34297735
John <jkforsythe@hotmail.com>
2019-12-02 09:30:46 +0000

    delete book function created

--- app.js
+++ app.js
@@ -44,6 +44,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete Book 
+UI.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -51,7 +58,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 }
 
-// Event Listeners
+// Event Listener for Add Book function
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -79,6 +86,20 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   ui.clearFields();
 
   }
+
+  e.preventDefault();
+});
+
+// Event listener for Delete function
+document.getElementById('book-list').addEventListener('click', function(e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message alert
+  ui.showAlert('Book Removed!!', 'success');
 
   e.preventDefault();
 });
